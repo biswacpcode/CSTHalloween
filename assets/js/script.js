4866b27963009ae8e1a2a91dcf1e5e60c5418d7e
Biswajit Rout <114167619+biswacpcode@users.noreply.github.com>
2023-10-30 14:39:39 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,7 @@ pos.addEventListener('mousemove', e=>{
   let x = e.clientX, y = e.clientY;
   flash.style.background = `radial-gradient(circle at ${x}px ${y}px , transparent 10%, rgba(0, 0, 0, 0.95) 30%)`
 })
+pos.addEventListener('touchmove', e=>{
+  let x = e.clientX, y = e.clientY;
+  flash.style.background = `radial-gradient(circle at ${x}px ${y}px , transparent 10%, rgba(0, 0, 0, 0.95) 30%)`
+})
